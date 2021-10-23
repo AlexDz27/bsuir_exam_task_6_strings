ffcf1e72c82dbc6d520c9c5403fbd1af929a7d20
Alex Dzyuba <alexeydzyuba27@gmail.com>
2021-10-23 10:00:50 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+  const int NUMBERS_COUNT = 7;
+
+  string numbersString = "42 30 55 66 9 122 47";
+  int numbers[NUMBERS_COUNT];
+
+  stringstream StringStream(numbersString);
+  string numberTemporaryHolder;
+  for (int i = 0; StringStream >> numberTemporaryHolder; i++) {
+    numbers[i] = stoi(numberTemporaryHolder);
+  }
+
+  cout << "Even numbers: ";
+  for (int num : numbers) {
+    if (num % 2 != 0) continue;
+
+    cout << num << " ";
+  }
+
+  return 0;
+}
